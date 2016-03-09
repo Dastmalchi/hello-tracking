1bcc8d2636a24eecc2f410e7c90a49d1a0ca5b5b
Daniel Smith <daniel@dastmalchi.com>
2016-03-09 14:27:17 -0800

    Adds estimated date for dhl-global-mail

--- project/apps/status/views.py
+++ project/apps/status/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.conf import settings
 
 import utils
-from datetime import datetime
+import datetime
 import aftership
 
 
@@ -28,11 +28,13 @@ def trackings(request, carrier_slug, tracking_number):
                 'active',
                 'shipment_pickup_date',
                 'shipment_type',
+                'destination_country_iso3',
                 'created_at',
                 'tag'])
 
     response = raw_tracking['tracking']
 
+    # Process checkpoints
     for checkpoint in response['checkpoints']:
             del checkpoint['created_at']
             del checkpoint['slug']
@@ -43,10 +45,30 @@ def trackings(request, carrier_slug, tracking_number):
             checkpoint['checkpoint_time'] = utils.format_date_for_display(
                 checkpoint['checkpoint_time'])
             checkpoints.append(checkpoint)
-
     checkpoints.reverse()
     response['checkpoints'] = checkpoints
 
+    # Fix shipment_pickup_date
+    if response['shipment_pickup_date'] is not None:
+        response['shipment_pickup_date'] = \
+            datetime.datetime.strptime(response['shipment_pickup_date'],
+                                       '%Y-%m-%dT%H:%M:%S')
+
+    # Guess the expected delivery for dhl-global-mail
+    # when it isn't defined and we have shipment_pickup_date
+    if carrier_slug == 'dhl-global-mail' and \
+            response['shipment_pickup_date'] is not None and \
+            response['expected_delivery'] is None:
+        if response['destination_country_iso3'] == 'USA':
+            # USA is 7 days
+            response['expected_delivery'] = response['shipment_pickup_date'] \
+                + datetime.timedelta(days=7)
+        else:
+            # International is 24 days
+            response['expected_delivery'] = response['shipment_pickup_date'] \
+                + datetime.timedelta(days=24)
+
+    # Format expected_delivery for display
     if response['expected_delivery'] is not None:
         response['expected_delivery'] = {
             'dow': response['expected_delivery'].strftime("%A"),
@@ -54,14 +76,12 @@ def trackings(request, carrier_slug, tracking_number):
             'day': response['expected_delivery'].strftime("%d")
         }
 
+    # Format created_at for display
     response['created_at'] = utils.format_date_for_display(
         response['created_at'])
 
-    if response['shipment_pickup_date'] is not None:
-        response['shipment_pickup_date'] = \
-            datetime.strptime(response['shipment_pickup_date'],
-                              '%Y-%m-%dT%H:%M:%S')
-        response['shipment_pickup_date'] = utils.format_date_for_display(
-            response['shipment_pickup_date'])
+    # Cleanup response
+    del response['shipment_pickup_date']
+    del response['destination_country_iso3']
 
     return response
